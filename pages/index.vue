b24d82154ae9f643945a870ab4659e9a19308833
ongcg <73372833+ongcg@users.noreply.github.com>
2022-08-07 12:12:16 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+    <div class="m-auto">
+      <h1 class="text-5xl">testing</h1>
+      <p class="mt-5">Turn your ideas into ......</p>
+    </div>
+  </div>
+</template>
