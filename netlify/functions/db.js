b2b2b4a3f3a3f5ba76a63cf1d5b9403731aeff56
ongcg <chengee1005@gmail.com>
2022-08-27 17:36:01 +0800

    Database Functions 2

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -24,7 +24,7 @@ exports.handler = async function (event, context) {
         });
 
         return {
-            statusCode: 200
+            statusCode: 200,
             body: JSON.stringify({
                 message: 'Document added successfully - ' + response.id,
          }),
