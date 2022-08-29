a2d5d2034eae1eb9cf4f1a20148ed0f836b400c8
ongcg <chengee1005@gmail.com>
2022-08-29 22:43:10 +0800

    modify yarn install

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = JSON.parse(process.env.FirebaseCredential);
+const serviceAccount = JSON.parse(process.env.firebaseCredential);
 
 exports.handler = async function (event, context) {
     let app;
